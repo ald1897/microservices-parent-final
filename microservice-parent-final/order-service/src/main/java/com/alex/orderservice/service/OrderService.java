234294fc52c6dc3e46a18842258f62fecf9d06ba
Alex Dickinson <alexdickinson04@gmail.com>
2023-06-16 14:45:31 -0400

    cleaned up methods in order service, next up figure out how to authenticate with Inventory Service with KC

--- microservice-parent-final/order-service/src/main/java/com/alex/orderservice/service/OrderService.java
+++ microservice-parent-final/order-service/src/main/java/com/alex/orderservice/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.alex.orderservice.service;
 
-//import com.alex.inventoryservice.service.InventoryService;
 import com.alex.orderservice.dto.*;
 import com.alex.orderservice.model.Order;
 import com.alex.orderservice.model.OrderLineItems;
@@ -15,50 +14,64 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
 public class OrderService {
-
     private final OrderRepository orderRepository;
-//    private final InventoryService inventoryService;
-
-
     private final WebClient.Builder webClientBuilder;
 
-    //placeOrder Method takes in the OrderRequest object passed in from the controller
     public void placeOrder(OrderRequest orderRequest) {
+        //placeOrder Method takes in the OrderRequest object passed in from the controller
         log.info("Inside placeOrder of OrderService");
         log.info("OrderRequest: " + orderRequest);
+
         //Create new order object, set the order number, and set the orderLineItemsList
         Order order = createOrder(orderRequest);
-        // add logging
         log.info("Order: " + order);
+
         // Create a list of skuCodes from the orderLineItemsList
         List<String> skuCodes = createSkuCodeList(order);
-        // add logging
         log.info("SkuCodes: " + skuCodes);
+
         // Create a list of qtys from the orderLineItemsList
         List<Integer> qtys = createQtyList(order);
-        // add logging
         log.info("Qtys: " + qtys);
+
         //Create a method that creates 2 iterators and a map to house the skuCode and qty
         Multimap<String, Integer> map = createIteratorMap(order, skuCodes, qtys);
-        // add logging
         log.info("Multimap: " + map);
-        // Create a method to Update Inventory Service by reducing Quantity by amount that was ordered for each skuCode
-        updateInventory(order);
-        // add logging
-        log.info("Updating Inventory from Order Service");
-        // Create a method to save the order
-        saveOrder(order);
-        // add logging
-        log.info("All Items in stock, saving order.");
-        log.info("Order Number: {} was placed", order.getOrderNumber());
-        // Create an array of inventoryResponses by executing a GET request to http://localhost:8082/api/inventory?skuCode=x&qty=y
-        InventoryResponse[] inventoryResponseArray = new InventoryResponse[0];
+
+        // Create a method to check if the order skuCodes are valid and if the qtys are valid
+        InventoryResponse[] validOrder = checkIfValidOrder(order, skuCodes, qtys);
+        log.info("Checking if order is valid & if all items are in stock");
+
+        // if the validOrder is returned then check allItemsInStock
+        if (validOrder.length > 0) {
+            // Create a method to check if all items are in stock
+            boolean allItemsInStock = checkIfAllItemsInStock(validOrder);
+            log.info("All items in stock: " + allItemsInStock);
+
+            // Create a method to update the inventory
+            if (allItemsInStock) {
+                log.info("All Items in stock, Updating inventory");
+                updateInventory(map);
+
+                log.info("Saving Order.");
+                saveOrder(order);
+
+                log.info("Order Number: {} was placed", order.getOrderNumber());
+
+            } else {log.info("Order is invalid, not saving order.");}
+
+        } else {log.info("Order is invalid, not saving order.");}
+    }
+
+    private  InventoryResponse[] checkIfValidOrder(Order order, List<String> skuCodes, List<Integer> qtys) {
+        // Create an array of inventoryResponses by executing a GET request to http://localhost:8080/api/inventory?skuCode=x&qty=y
+        log.info("Checking if order is valid & if all items are in stock");
+       InventoryResponse[] inventoryResponseArray = new InventoryResponse[0];
         try {
             inventoryResponseArray = webClientBuilder.build().get()
                     .uri("http://inventory-service/api/inventory",
@@ -69,52 +82,29 @@ public class OrderService {
         } catch (Exception e) {
             //  Block of code to handle errors
             throw new IllegalArgumentException("Sku Code does not exist. Try again later.");
-
-
         }
-//         Check if all items in the array are isInStock = TRUE
-        assert inventoryResponseArray != null;
-        for (InventoryResponse inventoryResponse : inventoryResponseArray) {
-            if (!inventoryResponse.isInStock()) {
-                log.info("Item is not in stock");
-                throw new IllegalArgumentException("Item is not in stock. Try again later.");
-            }
-        }
-        boolean allProductsInStock = true;
-
-        // IF all items are in stock for the required amount, save the order and update the inventory amounts for the items
-        if (allProductsInStock) {
-            orderRepository.save(order);
-            log.info("All Items in stock, saving order.");
-            log.info("Order Number: {} was placed", order.getOrderNumber());
-
-            log.info("Updating Inventory from Order Service");
-            // Iterate through multimap list so skuCode and qty are lined up
-            for (Map.Entry entry : map.entries()) {
-
-                // Store skuCode and qty in a variable
-                String skuCode = String.valueOf(entry.getKey());
-                Integer qty = (Integer) entry.getValue();
-
-                // Send a post request to inventory service with the skuCode and qty to be deducted
-                log.info("Hitting inventory endpoint inventory-service/api/inventory...");
-                InventoryUpdate inventoryUpdate = webClientBuilder.build().post()
-                        .uri("http://inventory-service/api/inventory",
-                                uriBuilder -> uriBuilder.queryParam("skuCode", skuCode).queryParam("qty", qty).build())
-                        .retrieve()
-                        .bodyToMono(InventoryUpdate.class)
-                        .block();
-            }
-
+        if (inventoryResponseArray.length == 0) {
+            throw new IllegalArgumentException("Inventory Response Empty. Try again later.");
         } else {
-            throw new IllegalArgumentException("Products are out of stock. Check Later");
+            return inventoryResponseArray;}
+    }
+
+    private boolean checkIfAllItemsInStock(InventoryResponse[] validOrder) {
+        // Create a boolean to check if all items are in stock
+        boolean allItemsInStock = true;
+        // Iterate through the inventoryResponseArray to check if all items are in stock
+        for (InventoryResponse inventoryResponse : validOrder) {
+            if (inventoryResponse.isInStock() == false) {
+                allItemsInStock = false;
+            }
         }
-
-
+        return allItemsInStock;
     }
 
     private void saveOrder(Order order) {
-
+        log.info("All Items in stock, saving order.");
+        orderRepository.save(order);
+        log.info("Order Number: {} was placed", order.getOrderNumber());
     }
 
     private Multimap<String, Integer> createIteratorMap(Order order, List<String> skuCodes, List<Integer> qtys) {
@@ -136,21 +126,25 @@ public class OrderService {
                 .toList();
     }
 
-    private void updateInventory(Order order) {
-        List<String> skuCodes = createSkuCodeList(order);
-        List<Integer> qtys = order.getOrderLineItemsList().stream()
-                .map(OrderLineItems::getQty)
-                .toList();
+    private void updateInventory(Multimap<String, Integer> map) {
+        // Iterate through multimap list so skuCode and qty are lined up
+        for (Map.Entry entry : map.entries()) {
 
-        // Create iterators to loop through each list in sync
-        Iterator<String> it1 = skuCodes.iterator();
-        Iterator<Integer> it2 = qtys.iterator();
+            // Store skuCode and qty in a variable
+            String skuCode = String.valueOf(entry.getKey());
+            Integer qty = (Integer) entry.getValue();
 
-        // Store iterator data here in key/value pairs
-        Multimap<String, Integer> map = ArrayListMultimap.create();
-        while (it1.hasNext() && it2.hasNext()) {
-            map.put(it1.next(), it2.next());
+            // Send a post request to inventory service with the skuCode and qty to be deducted
+            log.info("Hitting inventory endpoint inventory-service/api/inventory...");
+            InventoryUpdate inventoryUpdate = webClientBuilder.build().post()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCode).queryParam("qty", qty).build())
+                    .retrieve()
+                    .bodyToMono(InventoryUpdate.class)
+                    .block();
+            log.info("Inventory Update: " + inventoryUpdate);
         }
+        log.info("Inventory Updated");
     }
 
     private List<String> createSkuCodeList(Order order) {
