0e8a9f43e972e0c3c356923c72fa2cd4dca0aca5
Alex Dickinson <alexdickinson04@gmail.com>
2023-06-16 12:04:20 -0400

    Optimizing code and fixing order service

--- microservice-parent-final/order-service/src/main/java/com/alex/orderservice/service/OrderService.java
+++ microservice-parent-final/order-service/src/main/java/com/alex/orderservice/service/OrderService.java
@@ -30,69 +30,56 @@ public class OrderService {
 
     //placeOrder Method takes in the OrderRequest object passed in from the controller
     public void placeOrder(OrderRequest orderRequest) {
-        //Then it creates a new Order Object called order
-        Order order = new Order();
-        //Then it sets the order number with order.setOrderNumber method
-        order.setOrderNumber(UUID.randomUUID().toString());
-        // Then we have to map the Order line items coming in from the OrderRequest to the OrderLineItems(model) object
-        //orderRequest.getOrderLineItemsDtoList() gets the list of items in an order
-        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
-                // stream the list
-                .stream()
-                // Map each item in the list using the mapToDto method
-                .map(this::mapToDto)
-                //Add the items to a list
-                .toList();
+        log.info("Inside placeOrder of OrderService");
+        log.info("OrderRequest: " + orderRequest);
+        //Create new order object, set the order number, and set the orderLineItemsList
+        Order order = createOrder(orderRequest);
+        // add logging
+        log.info("Order: " + order);
+        // Create a list of skuCodes from the orderLineItemsList
+        List<String> skuCodes = createSkuCodeList(order);
+        // add logging
+        log.info("SkuCodes: " + skuCodes);
+        // Create a list of qtys from the orderLineItemsList
+        List<Integer> qtys = createQtyList(order);
+        // add logging
+        log.info("Qtys: " + qtys);
+        //Create a method that creates 2 iterators and a map to house the skuCode and qty
+        Multimap<String, Integer> map = createIteratorMap(order, skuCodes, qtys);
+        // add logging
+        log.info("Multimap: " + map);
+        // Create a method to Update Inventory Service by reducing Quantity by amount that was ordered for each skuCode
+        updateInventory(order);
+        // add logging
+        log.info("Updating Inventory from Order Service");
+        // Create a method to save the order
+        saveOrder(order);
+        // add logging
+        log.info("All Items in stock, saving order.");
+        log.info("Order Number: {} was placed", order.getOrderNumber());
+        // Create an array of inventoryResponses by executing a GET request to http://localhost:8082/api/inventory?skuCode=x&qty=y
+        InventoryResponse[] inventoryResponseArray = new InventoryResponse[0];
+        try {
+            inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).queryParam("qty", qtys).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+        } catch (Exception e) {
+            //  Block of code to handle errors
+            throw new IllegalArgumentException("Sku Code does not exist. Try again later.");
 
-        // Set the order.orderLineItemsList field to the list you just mapped
-        order.setOrderLineItemsList(orderLineItems);
 
-        // Create list of Sku Codes present in the order
-        List<String> skuCodes = order.getOrderLineItemsList().stream()
-                .map(OrderLineItems::getSkuCode)
-                .toList();
-
-        // Update Inventory Service by reducing Quantity by amount that was ordered
-        List<Integer> qtys = order.getOrderLineItemsList().stream()
-                .map(OrderLineItems::getQty)
-                .toList();
-
-        // Create iterators to loop through each list in sync
-        Iterator<String> it1 = skuCodes.iterator();
-        Iterator<Integer> it2 = qtys.iterator();
-
-        // Store iterator data here in key/value pairs
-        Multimap<String, Integer> map = ArrayListMultimap.create();
-        while (it1.hasNext() && it2.hasNext()) {
-            map.put(it1.next(), it2.next());
         }
-
-//        log.info("Inventory MultiMap: " + map);
-
-
-//        // Create an array of inventoryResponses by executing a GET request to http://localhost:8082/api/inventory?skuCode=x&qty=y
-//        InventoryResponse[] inventoryResponseArray = new InventoryResponse[0];
-//        try {
-//            inventoryResponseArray = webClientBuilder.build().get()
-//                    .uri("http://inventory-service/api/inventory",
-//                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).queryParam("qty", qtys).build())
-//                    .retrieve()
-//                    .bodyToMono(InventoryResponse[].class)
-//                    .block();
-//        } catch (Exception e) {
-//            //  Block of code to handle errors
-//            throw new IllegalArgumentException("Sku Code does not exist. Try again later.");
-//
-//
-//        }
-        // Check if all items in the array are isInStock = TRUE
-//        assert inventoryResponseArray != null;
-//        for (InventoryResponse inventoryResponse : inventoryResponseArray) {
-//            if (!inventoryResponse.isInStock()) {
-//                log.info("Item is not in stock");
-//                throw new IllegalArgumentException("Item is not in stock. Try again later.");
-//            }
-//        }
+//         Check if all items in the array are isInStock = TRUE
+        assert inventoryResponseArray != null;
+        for (InventoryResponse inventoryResponse : inventoryResponseArray) {
+            if (!inventoryResponse.isInStock()) {
+                log.info("Item is not in stock");
+                throw new IllegalArgumentException("Item is not in stock. Try again later.");
+            }
+        }
         boolean allProductsInStock = true;
 
         // IF all items are in stock for the required amount, save the order and update the inventory amounts for the items
@@ -124,6 +111,63 @@ public class OrderService {
         }
 
 
+    }
+
+    private void saveOrder(Order order) {
+
+    }
+
+    private Multimap<String, Integer> createIteratorMap(Order order, List<String> skuCodes, List<Integer> qtys) {
+        // Create iterators to loop through each list in sync
+        Iterator<String> it1 = skuCodes.iterator();
+        Iterator<Integer> it2 = qtys.iterator();
+
+        // Store iterator data here in key/value pairs
+        Multimap<String, Integer> map = ArrayListMultimap.create();
+        while (it1.hasNext() && it2.hasNext()) {
+            map.put(it1.next(), it2.next());
+        }
+        return map;
+    }
+
+    private List<Integer> createQtyList(Order order) {
+        return order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getQty)
+                .toList();
+    }
+
+    private void updateInventory(Order order) {
+        List<String> skuCodes = createSkuCodeList(order);
+        List<Integer> qtys = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getQty)
+                .toList();
+
+        // Create iterators to loop through each list in sync
+        Iterator<String> it1 = skuCodes.iterator();
+        Iterator<Integer> it2 = qtys.iterator();
+
+        // Store iterator data here in key/value pairs
+        Multimap<String, Integer> map = ArrayListMultimap.create();
+        while (it1.hasNext() && it2.hasNext()) {
+            map.put(it1.next(), it2.next());
+        }
+    }
+
+    private List<String> createSkuCodeList(Order order) {
+        return order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+    }
+
+    private Order createOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+        return order;
     }
 
     public List<OrderResponse> getAllOrders() {
